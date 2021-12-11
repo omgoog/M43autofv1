6caa762c420a341775ff8dcf2e1062fddb0c91c2
samesame37916 <89601803+samesame37916@users.noreply.github.com>
2021-12-11 14:21:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
-    START_TXT = """<b>Hello {}</b>
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 
+    START_TXT = """<b>Hello</b> {}
+MY NAME IS <a href=https://t.me/{}>{}</a>, 
 
 <i>Iam A Simple Auto Filter + Movie Search + Manual Filter Bot. I Can Provide Movies In Telegram Groups. You Can Search Movies Via Inline. I Can Also Add Filters In Telegram Groups.  Just Add Me To Your Group And Enjoy</i>
 
